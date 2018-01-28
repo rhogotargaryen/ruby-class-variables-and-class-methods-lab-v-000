5b35040485c10048319f37769b8e49931fbb76b7
Derek Haller <dhaller.personal@gmail.com>
2018-01-28 20:58:49 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,33 @@
+class Song
+  attr_accessor :name, :artist, :genre
+  @@count = 0
+  @@artists = []
+  @@genres = []
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@genres << genre
+    @@artists << artist
+    @@count += 1
+  end
+  def self.count
+    @@count
+  end
+  def self.artists
+    @@artists.uniq
+  end
+  def self.genres
+    @@genres.uniq
+  end
+  def self.genre_count
+    genre_hash = {}
+    gen_array = @@genre.sort
+    gen_array.each do |gen|
+      if genre_hash[gen] == false
+        genre_hash[gen] => 1
+      else
+        genre_hash[gen] += 2
+      end
+    end
+end
