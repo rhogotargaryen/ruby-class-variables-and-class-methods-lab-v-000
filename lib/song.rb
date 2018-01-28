928f3bcae01be097294661d946d55b0d2a9bb259
Derek Haller <dhaller.personal@gmail.com>
2018-01-28 20:59:03 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -25,7 +25,7 @@ class Song
     gen_array = @@genre.sort
     gen_array.each do |gen|
       if genre_hash[gen] == false
-        genre_hash[gen] => 1
+        genre_hash[gen] = 1
       else
         genre_hash[gen] += 2
       end
